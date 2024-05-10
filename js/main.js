fe75b55c29e919d1ec4f0ffa0b3013d4ff82f9c5
isaacZEK <isaac.chama32@gmail.com>
2024-05-10 07:38:10 +0200

    finished the web and only left with making it responsive across different devices and align them properly

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//change developer messages
+
 let words = document.querySelectorAll(".word");
 
 words.forEach((word) => {
@@ -42,22 +44,74 @@ changeText();
 setInterval(changeText, 5000); // Changed interval timing for testing
 
 
-window.addEventListener("scroll",activemenu);
-
 const header  = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky",window.scrollY > 50)
-})
+});
 
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector("navbar");
 
-menuIcon.onclick = () =>{
-    menuIcon.checkVisibilitylassList.toggle("bx-x");
-    navbar.classList.toggle("open");
-    
+
+//mix it up portfolio section
+var mixer = mixitup('.portfolio-gallery');
+
+//active menu
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelector("section");
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+    }
 }
 
+let active = activeMenu();
+window.addEventListener("scroll", active);
 
-//mi it up portfolio section
-var mixer = mixitup('.portfolio-gallery');
+
+// Toggle navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector("#main-nav");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("open");
+}
+
+// Close the navbar when clicking outside of it
+document.addEventListener("click", function(event){
+    if (!navbar.contains(event.target) && !menuIcon.contains(event.target)) { 
+        // Fix typo: event instead of events
+        menuIcon.classList.remove("bx-x");
+        navbar.classList.remove("open");
+    }
+});
+
+
+//close the navbar when scrolling
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("open"); 
+}
+
+//parallax
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+            entry.target.classList.remove("show-items");
+        }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el) => observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el) => observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el) => observer.observe(el));
